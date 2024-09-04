63ea427e87699b3afddfa3c9da058dd1f2bd9551
Cosmic Process <126974221+cosmicproc@users.noreply.github.com>
2024-09-04 10:49:05 +0300

    Add source information and update deps

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -4,7 +4,9 @@ export default function Footer() {
     return (
         <footer className="text-center my-8 mt-auto print:hidden">
             Created by{' '}
-            <Link href="https://github.com/cosmicproc">cosmicproc</Link>
+            <Link href="https://github.com/cosmicproc">cosmicproc</Link> |
+            Source on{' '}
+            <Link href="https://github.com/cosmicproc/outrecta/">GitHub</Link>
         </footer>
     );
 }
